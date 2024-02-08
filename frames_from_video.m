741b0b388dd07508e5efeb97a985253b81b77eaf
AMRIT PUHAN <amritpuhan07@gmail.com>
2024-02-08 14:53:46 -0500

    Update frames_from_video.m

--- frames_from_video.m
+++ frames_from_video.m
@@ -1,23 +1,26 @@
-// Clear the command window, workspace, and close all figures
+% Clear the command window, workspace, and close all figures
 clc;
 clear all;
 close all;
 
-// Start the timer
+% Start the timer
 tic;
 
-// Create a VideoReader object to read the video file
-vid=VideoReader('Path to .avi video');
+% Create a VideoReader object to read the video file
+vid = VideoReader('Path to .avi video');
 
-// Get the total number of frames in the video
+% Get the total number of frames in the video
 numFrames = vid.NumberOfFrames;
-n=numFrames;
+n = numFrames;
 
-// Iterate through each frame of the video
+% Iterate through each frame of the video
 for i = 1:1:n
-  // Read the current frame
-  frames = read(vid,i);
+  % Read the current frame
+  frames = read(vid, i);
   
-  // Save the current frame as an image file
-  imwrite(frames,['Path to wherever you want to save '.jpg']);
+  % Construct the path and filename for saving the current frame
+  outputFileName = sprintf('Path to wherever you want to save/Frame_%d.jpg', i);
+  
+  % Save the current frame as an image file
+  imwrite(frames, outputFileName);
 end
